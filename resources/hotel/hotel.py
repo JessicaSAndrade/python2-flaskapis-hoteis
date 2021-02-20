d5a5e94cdce6e9c280fa280fcc39e6311fa17b43
jessica <jessica.1999.2009@gmail.com>
2021-02-20 15:33:01 -0300

    finalização do fluxo de apis

--- resources/hotel/hotel.py
+++ resources/hotel/hotel.py
@@ -2,31 +2,20 @@ from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
 
-hoteis = [
-    {"hotel_id": "copacabana", "nome": "Copacabana Palace", 
-    "estrelas": 5, "diaria":1000, "cidade": "Rio de Janeio"},
-
-    {"hotel_id": "gravata", "nome": "Hotel Gravatá",
-    "estrelas": 3.8, "diaria": 400, "cidade": "Gravatá"},
-
-    {"hotel_id": "noronha", "nome": "Hotel de Noronha",
-    "estrelas": 4.5, "diaria": 300.25, "cidade": "Noronha"}
-]
-
 # Endpoint de listagem de todos os hoteis
 class Hoteis(Resource):
     def get(self):
-        return {"hoteis": hoteis}
+        return {"hoteis": [hotel.json() for hotel in HotelModel.query.all()]}
 
 # Endpoints de criaçõa, edição, remoção e pesquisa de hoteis
 class Hotel(Resource):
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument("nome")
-    argumentos.add_argument("estrelas")
-    argumentos.add_argument("diaria")
-    argumentos.add_argument("cidade")
+    argumentos.add_argument('nome', type=str, required=True, help="O campo nome é obrigatório e não pode ficar em branco")
+    argumentos.add_argument('estrelas', type=float, required=True, help="O campo estrelas é obrigatório e não pode ficar em branco")
+    argumentos.add_argument('diaria', type=float, required=True, help="O campo diária é obrigatório e não pode ficar em branco")
+    argumentos.add_argument('cidade', type=str, required=True, help="O campo cidade é obrigatório e não pode ficar em branco")
 
-    # Find by id
+        # Find by id
     def get(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
@@ -36,35 +25,39 @@ class Hotel(Resource):
     # Create Hotel
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
-            return {"messase": "O Hotel_id '{}' já existe, insira outro".format(hotel_id)}, 400
+            return {"message": "Hotel id '{}' ja existe".format(hotel_id)}, 400
+
         dados = Hotel.argumentos.parse_args()
         hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
+        try:
+            hotel.save_hotel()
+        except:
+              return ("message: Erro interno no servidor, consulte o suporte!"), 500
         return hotel.json()
-
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel["hotel_id"] == hotel_id:
-                return hotel
-        return None
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        
         hotel_encontrado = HotelModel.find_hotel(hotel_id)
         if hotel_encontrado:
             hotel_encontrado.update_hotel(**dados)
             hotel_encontrado.save_hotel()
             return hotel_encontrado.json(), 200
-        
+
         hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
+        try:
+            hotel.save_hotel()
+        except:
+              return ("message: Erro interno no servidor, consulte o suporte!"), 500
         return hotel.json(), 201
 
     def delete(self, hotel_id):
-        exist_hotel = Hotel.find_hotel(hotel_id)
-        if exist_hotel:
-            hoteis.remove(exist_hotel)
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try:
+                hotel.delete_hotel()
+            except:
+                return ("message: Erro interno no servidor, consulte o suporte!"), 500
             return ("message: Deletado com sucesso!"), 200
         return ("message: id não encontrado"), 404
+
         
